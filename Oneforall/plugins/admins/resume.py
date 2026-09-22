d6cb475473a0c2e81a679905c9d1c7932e22e61e
binnerjoylong-cyber <binnerjoylong@gmail.com>
2026-09-23 04:30:48 +0530

    Update resume.py

--- Oneforall/plugins/admins/resume.py
+++ Oneforall/plugins/admins/resume.py
@@ -7,6 +7,7 @@ from Oneforall.core.call import Hotty
 from Oneforall.utils.database import is_music_playing, music_on
 from Oneforall.utils.decorators import AdminRightsCheck
 from Oneforall.utils.inline import close_markup
+from Oneforall.utils.inline.rich import set_now_playing_state
 
 
 @app.on_message(filters.command(["resume", "cresume"]) & filters.group & ~BANNED_USERS)
@@ -16,6 +17,7 @@ async def resume_com(cli, message: Message, _, chat_id):
         return await message.reply_text(_["admin_3"])
     await music_on(chat_id)
     await Hotty.resume_stream(chat_id)
+    await set_now_playing_state(chat_id, playing=True)
     await message.reply_text(
         _["admin_4"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
